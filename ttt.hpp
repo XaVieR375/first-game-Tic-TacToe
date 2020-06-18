89321f41cdaff5f982426f51968a0fa5f1aa3446
XaVieR375 <67128892+XaVieR375@users.noreply.github.com>
2020-06-18 19:56:26 -0400

    Create ttt.hpp

---
+++ ttt.hpp
@@ -0,0 +1,6 @@
+//#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+void tictactoegame();
+void display(vector<string> positions = {"0","1","2","3","4","5","6","7","8", "9"});
